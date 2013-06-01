e10c040b98655bc617aba48229bc725c75a029d4
XXMrHyde <xxmrhyde@AwesomeMashine.(none)>
2013-06-02 01:08:03 +0200

    Add statusbar transparency mode, statusbar style menu, and statusbar color code cleanup
    Statusbar transparency mode:
    - home
    - keyguard and home (default)
    - always
    
    Statusbar style menu:
    - reset
    
    Statusbar transparency mode and statusbar style menu is ported from SlimRoms,
    Statusbar color code cleanup is ported/inspired from SlimRoms:
    
    https://github.com/SlimRoms/packages_apps_Settings/commit/108f991efa82ece17c97cb5a49db63b984dfb38e

--- src/com/android/settings/darkjelly/StatusBarStyle.java
+++ src/com/android/settings/darkjelly/StatusBarStyle.java
@@ -17,11 +17,15 @@
 package com.android.settings.darkjelly;
 
 import android.os.Bundle;
+import android.preference.ListPreference; 
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.android.settings.darkjelly.colorpicker.ColorPickerPreference;
 import com.android.settings.R;
@@ -30,24 +34,40 @@ import com.android.settings.widget.SeekBarPreference;
 
 public class StatusBarStyle extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
 
+    private static final String TAG = "StatusBarStyle"; 
     private static final String PREF_STATUS_BAR_COLOR = "status_bar_color";
     private static final String PREF_STATUS_BAR_ALPHA = "status_bar_alpha";
+    private static final String PREF_STATUS_BAR_ALPHA_MODE = "status_bar_alpha_mode";
 
     private ColorPickerPreference mStatusBarColor;
     private SeekBarPreference mStatusbarTransparency;
+    private ListPreference mStatusbarAlphaMode; 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        refreshSettings();
+
+    }
+
+    public void refreshSettings() {
+        PreferenceScreen prefs = getPreferenceScreen();
+        if (prefs != null) {
+            prefs.removeAll();
+        }
 
         addPreferencesFromResource(R.xml.status_bar_style);
 
         PreferenceScreen prefSet = getPreferenceScreen();
 
         mStatusBarColor = (ColorPickerPreference) prefSet.findPreference(PREF_STATUS_BAR_COLOR);
-        mStatusbarTransparency = (SeekBarPreference) findPreference(PREF_STATUS_BAR_ALPHA); 
+        mStatusbarTransparency = (SeekBarPreference) prefSet.findPreference(PREF_STATUS_BAR_ALPHA);
+        mStatusbarAlphaMode = (ListPreference) prefSet.findPreference(PREF_STATUS_BAR_ALPHA_MODE); 
 
         mStatusBarColor.setOnPreferenceChangeListener(this);
+        int intColor = Settings.System.getInt(getActivity().getContentResolver(),
+                    Settings.System.STATUS_BAR_COLOR, 0xff000000); 
+        mStatusBarColor.setNewPreviewColor(intColor);
 
         float statBarTransparency = 0.0f;
         try{
@@ -59,7 +79,44 @@ public class StatusBarStyle extends SettingsPreferenceFragment implements Prefer
         }
         mStatusbarTransparency.setProperty(Settings.System.STATUS_BAR_ALPHA);
         mStatusbarTransparency.setInitValue((int) (statBarTransparency * 100));
-        mStatusbarTransparency.setOnPreferenceChangeListener(this); 
+        mStatusbarTransparency.setOnPreferenceChangeListener(this);
+
+        int statusbarAlphaMode = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.STATUS_BAR_ALPHA_MODE, 1);
+        mStatusbarAlphaMode.setValue(String.valueOf(statusbarAlphaMode));
+        mStatusbarAlphaMode.setSummary(mStatusbarAlphaMode.getEntry());
+        mStatusbarAlphaMode.setOnPreferenceChangeListener(this);
+
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.status_bar_style, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.reset:
+                Settings.System.putInt(getActivity().getContentResolver(),
+                        Settings.System.STATUS_BAR_COLOR, 0xff000000);
+                Settings.System.putFloat(getActivity().getContentResolver(),
+                       Settings.System.STATUS_BAR_ALPHA, 0.0f);
+                Settings.System.putInt(getActivity().getContentResolver(),
+                        Settings.System.STATUS_BAR_ALPHA_MODE, 1);
+                refreshSettings();
+                return true;
+             default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
+            Preference preference) {
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
@@ -75,13 +132,19 @@ public class StatusBarStyle extends SettingsPreferenceFragment implements Prefer
                     Settings.System.STATUS_BAR_ALPHA,
                     valStat / 100);
             return true; 
+        } else if (preference == mStatusbarAlphaMode) {
+            int statusbarAlphaMode = Integer.valueOf((String) newValue);
+            int index = mStatusbarAlphaMode.findIndexOfValue((String) newValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.STATUS_BAR_ALPHA_MODE, statusbarAlphaMode);
+            mStatusbarAlphaMode.setSummary(mStatusbarAlphaMode.getEntries()[index]);
+            return true;
         }
         return false;
     }
 
     @Override
-    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
-            Preference preference) {
-        return super.onPreferenceTreeClick(preferenceScreen, preference);
-    }
+    public void onResume() {
+        super.onResume();
+    } 
 }
