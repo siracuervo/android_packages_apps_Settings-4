9cc5d672d75c553fa7266beca6d5de196ff1acf3
XXMrHyde <xxmrhyde@gmx.co.uk>
2013-07-31 23:52:59 +0200

    Settings: Add an option to enable Dual pane mode
    Change-Id: I910906696bf60f0eb0de69733b80aaa245dd301d

---
+++ src/com/android/settings/darkjelly/General.java
@@ -0,0 +1,68 @@
+/*
+* Copyright (C) 2013 Dark Jelly
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.settings.darkjelly;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.provider.Settings;
+
+import com.android.settings.Utils;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class General extends SettingsPreferenceFragment {
+
+    private static final String TAG = "General";
+
+    private static final String PREF_DUAL_PANE = "dual_pane";
+
+    private CheckBoxPreference mDualPane;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        addPreferencesFromResource(R.xml.general_settings);
+
+        mDualPane = (CheckBoxPreference) findPreference(PREF_DUAL_PANE);
+
+        mDualPane = (CheckBoxPreference) findPreference(PREF_DUAL_PANE);
+        boolean preferDualPane = getResources().getBoolean(
+                com.android.internal.R.bool.preferences_prefer_dual_pane);
+        boolean dualPaneMode = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.DUAL_PANE_PREFS, (preferDualPane ? 1 : 0)) == 1;
+        mDualPane.setChecked(dualPaneMode);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mDualPane) {
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.DUAL_PANE_PREFS,
+                    ((CheckBoxPreference) preference).isChecked() ? 1 : 0);
+            return true;
+        }
+
+        return false;
+    }
+}
