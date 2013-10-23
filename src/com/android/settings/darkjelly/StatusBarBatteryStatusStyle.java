ca5a1c89ab224b9246ef22676a1a84a088fb7b48
XXMrHyde <xxmrhyde@gmx.co.uk>
2013-10-24 01:10:20 +0200

    Settings, Circle battery: "Add customization for Circle dotted", (2/2):
    Included:
    
    - Custom lenght
    - Custom interval
    - Custom offset
    
    Change-Id: Ibf874c3d5cb9c4611fd75dd929c753df4cc9118e

--- src/com/android/settings/darkjelly/StatusBarBatteryStatusStyle.java
+++ src/com/android/settings/darkjelly/StatusBarBatteryStatusStyle.java
@@ -40,6 +40,9 @@ public class StatusBarBatteryStatusStyle extends SettingsPreferenceFragment impl
     private static final String PREF_BATT_STAT_STYLE = "battery_status_style";
     private static final String PREF_BATT_STAT_SHOW_TEXT = "battery_status_show_text";
     private static final String PREF_BATT_STAT_CIRCLE_DOTTED = "battery_circle_dotted";
+    private static final String PREF_BATT_STAT_CIRCLE_DOT_LENGTH = "battery_circle_dot_length";
+    private static final String PREF_BATT_STAT_CIRCLE_DOT_INTERVAL = "battery_circle_dot_interval";
+    private static final String PREF_BATT_STAT_CIRCLE_DOT_OFFSET = "battery_circle_dot_offset";
     private static final String PREF_BATT_STAT_FILL_COLOR = "battery_fill_color";
     private static final String PREF_BATT_STAT_EMPTY_COLOR = "battery_empty_color";
     private static final String PREF_BATT_STAT_TEXT_COLOR = "battery_text_color";
@@ -50,6 +53,9 @@ public class StatusBarBatteryStatusStyle extends SettingsPreferenceFragment impl
     private ListPreference mBatteryStatusStyle;
     private CheckBoxPreference mShowText;
     private CheckBoxPreference mCircleDotted;
+    private ListPreference mCircleDotLength;
+    private ListPreference mCircleDotInterval;
+    private ListPreference mCircleDotOffset;
     private ColorPickerPreference mFillColor;
     private ColorPickerPreference mEmptyColor;
     private ColorPickerPreference mBatteryTextColor;
@@ -76,6 +82,8 @@ public class StatusBarBatteryStatusStyle extends SettingsPreferenceFragment impl
                 Settings.System.STATUS_BAR_BATTERY_STATUS_ENABLE_THEME_DEFAULT, 1) == 1;
         boolean showtext = Settings.System.getInt(mResolver,
                 Settings.System.STATUS_BAR_BATTERY_STATUS_SHOW_TEXT, 1) == 1;
+        boolean isCircleDottedEnabled = Settings.System.getInt(mResolver,
+                Settings.System.STATUS_BAR_CIRCLE_DOTTED, 0) == 1;
         int intColor = 0xff0099cc;
         String hexColor = String.format("#%08x", (0xffffffff & 0xff0099cc));
 
@@ -99,10 +107,34 @@ public class StatusBarBatteryStatusStyle extends SettingsPreferenceFragment impl
 
         mCircleDotted = (CheckBoxPreference) findPreference(PREF_BATT_STAT_CIRCLE_DOTTED);
         if (batteryStatus == 3) {
-            mCircleDotted.setChecked((Settings.System.getInt(mResolver, Settings.System.STATUS_BAR_CIRCLE_DOTTED, 0) == 1));
+            mCircleDotted.setChecked(isCircleDottedEnabled);
             mCircleDotted.setOnPreferenceChangeListener(this);
         } else {
             removePreference(PREF_BATT_STAT_CIRCLE_DOTTED);
+        }
+
+        mCircleDotLength = (ListPreference) findPreference(PREF_BATT_STAT_CIRCLE_DOT_LENGTH);
+        mCircleDotInterval = (ListPreference) findPreference(PREF_BATT_STAT_CIRCLE_DOT_INTERVAL);
+        mCircleDotOffset = (ListPreference) findPreference(PREF_BATT_STAT_CIRCLE_DOT_OFFSET);
+        if (batteryStatus == 3 && isCircleDottedEnabled) {
+            int circleDotLength = Settings.System.getInt(mResolver, Settings.System.STATUS_BAR_CIRCLE_DOT_LENGTH, 3);
+            mCircleDotLength.setValue(String.valueOf(circleDotLength));
+            mCircleDotLength.setSummary(mCircleDotLength.getEntry());
+            mCircleDotLength.setOnPreferenceChangeListener(this);
+
+            int circleDotInterval = Settings.System.getInt(mResolver, Settings.System.STATUS_BAR_CIRCLE_DOT_INTERVAL, 2);
+            mCircleDotInterval.setValue(String.valueOf(circleDotInterval));
+            mCircleDotInterval.setSummary(mCircleDotInterval.getEntry());
+            mCircleDotInterval.setOnPreferenceChangeListener(this);
+
+            int circleDotOffset = Settings.System.getInt(mResolver, Settings.System.STATUS_BAR_CIRCLE_DOT_OFFSET, 0);
+            mCircleDotOffset.setValue(String.valueOf(circleDotOffset));
+            mCircleDotOffset.setSummary(mCircleDotOffset.getEntry());
+            mCircleDotOffset.setOnPreferenceChangeListener(this);
+        } else {
+            removePreference(PREF_BATT_STAT_CIRCLE_DOT_LENGTH);
+            removePreference(PREF_BATT_STAT_CIRCLE_DOT_INTERVAL);
+            removePreference(PREF_BATT_STAT_CIRCLE_DOT_OFFSET);
         }
 
         mFillColor = (ColorPickerPreference) findPreference(PREF_BATT_STAT_FILL_COLOR);
@@ -191,6 +223,9 @@ public class StatusBarBatteryStatusStyle extends SettingsPreferenceFragment impl
                 Settings.System.putInt(mResolver, Settings.System.STATUS_BAR_BATTERY_STATUS_STYLE, 3);
                 Settings.System.putInt(mResolver, Settings.System.STATUS_BAR_BATTERY_STATUS_SHOW_TEXT, 0);
                 Settings.System.putInt(mResolver, Settings.System.STATUS_BAR_CIRCLE_DOTTED, 0);
+                Settings.System.putInt(mResolver, Settings.System.STATUS_BAR_CIRCLE_DOT_LENGTH, 3);
+                Settings.System.putInt(mResolver, Settings.System.STATUS_BAR_CIRCLE_DOT_INTERVAL, 2);
+                Settings.System.putInt(mResolver, Settings.System.STATUS_BAR_CIRCLE_DOT_OFFSET, 0);
                 Settings.System.putInt(mResolver, Settings.System.STATUS_BAR_BATTERY_FILL_COLOR, 0xff0099cc);
                 Settings.System.putInt(mResolver, Settings.System.STATUS_BAR_BATTERY_EMPTY_COLOR, 0xff404040);
                 Settings.System.putInt(mResolver, Settings.System.STATUS_BAR_BATTERY_TEXT_COLOR, 0xffffffff);
@@ -202,6 +237,9 @@ public class StatusBarBatteryStatusStyle extends SettingsPreferenceFragment impl
                 Settings.System.putInt(mResolver, Settings.System.STATUS_BAR_BATTERY_STATUS_STYLE, 3);
                 Settings.System.putInt(mResolver, Settings.System.STATUS_BAR_BATTERY_STATUS_SHOW_TEXT, 1);
                 Settings.System.putInt(mResolver, Settings.System.STATUS_BAR_CIRCLE_DOTTED, 1);
+                Settings.System.putInt(mResolver, Settings.System.STATUS_BAR_CIRCLE_DOT_LENGTH, 3);
+                Settings.System.putInt(mResolver, Settings.System.STATUS_BAR_CIRCLE_DOT_INTERVAL, 2);
+                Settings.System.putInt(mResolver, Settings.System.STATUS_BAR_CIRCLE_DOT_OFFSET, 0);
                 Settings.System.putInt(mResolver, Settings.System.STATUS_BAR_BATTERY_FILL_COLOR, 0xff33b5e5);
                 Settings.System.putInt(mResolver, Settings.System.STATUS_BAR_BATTERY_EMPTY_COLOR, 0xff404040);
                 Settings.System.putInt(mResolver, Settings.System.STATUS_BAR_BATTERY_TEXT_COLOR, 0xffffffff);
@@ -239,6 +277,25 @@ public class StatusBarBatteryStatusStyle extends SettingsPreferenceFragment impl
         } else if (preference == mCircleDotted) {
             boolean value = (Boolean) newValue;
             Settings.System.putInt(mResolver, Settings.System.STATUS_BAR_CIRCLE_DOTTED, value ? 1 : 0);
+            refreshSettings();
+            return true;
+        } else if (preference == mCircleDotLength) {
+            int circleDotLength = Integer.valueOf((String) newValue);
+            index = mCircleDotLength.findIndexOfValue((String) newValue);
+            Settings.System.putInt(mResolver, Settings.System.STATUS_BAR_CIRCLE_DOT_LENGTH, circleDotLength);
+            mCircleDotLength.setSummary(mCircleDotLength.getEntries()[index]);
+            return true;
+        } else if (preference == mCircleDotInterval) {
+            int circleDotInterval = Integer.valueOf((String) newValue);
+            index = mCircleDotInterval.findIndexOfValue((String) newValue);
+            Settings.System.putInt(mResolver, Settings.System.STATUS_BAR_CIRCLE_DOT_INTERVAL, circleDotInterval);
+            mCircleDotInterval.setSummary(mCircleDotInterval.getEntries()[index]);
+            return true;
+        } else if (preference == mCircleDotOffset) {
+            int circleDotOffset = Integer.valueOf((String) newValue);
+            index = mCircleDotOffset.findIndexOfValue((String) newValue);
+            Settings.System.putInt(mResolver, Settings.System.STATUS_BAR_CIRCLE_DOT_OFFSET, circleDotOffset);
+            mCircleDotOffset.setSummary(mCircleDotOffset.getEntries()[index]);
             return true;
         } else if (preference == mFillColor) {
             hex = ColorPickerPreference.convertToARGB(Integer.valueOf(String.valueOf(newValue)));
