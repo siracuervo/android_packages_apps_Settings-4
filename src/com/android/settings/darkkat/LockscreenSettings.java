4bd6e38b300eec60db321683911295bc23aca211
XXMrHyde <xxmrhyde@gmx.co.uk>
2013-11-14 01:03:24 +0100

    Settings: Add battery status ring around unlock ring, (2/2):
    Included:
    
    - Add Settings header "Lockscreen"
    - Add Color picker preference,
      (by Sergey Margaritov, Daniel Nilsson, and Slimroms)
    - Add battery status ring around unlock ring:
      (Based on OmniRom)
    
      Included:
    
      - Enable/disable battery status ring
      - Option to load default color values from theme/system
      - Display battery status ring dotted/solid
      - Customize ring dots, (lenght, interval, offset)
      - Custom battery status ring color
      - Custom battery status ring charging color
    
    Colors:
    While charging, display the charging color.
    Without charging and battery status is below 14%, the color turns into red,
    otherwise, display the ring color.

---
+++ src/com/android/settings/darkkat/LockscreenSettings.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (C) 2012 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.darkkat;
+
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class LockscreenSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+    private static final String TAG = "LockscreenSettings";
+
+    private static final String KEY_LOCKSCREEN_SHOW_BATTERY_STATUS_RING = "lockscreen_show_battery_status_ring";
+
+    private CheckBoxPreference mShowBatteryStatusRing;
+
+    private boolean mIsPrimary;
+
+    private ContentResolver mResolver;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        refreshSettings();
+    }
+
+    public void refreshSettings() {
+        PreferenceScreen prefs = getPreferenceScreen();
+        if (prefs != null) {
+            prefs.removeAll();
+        }
+
+        addPreferencesFromResource(R.xml.lockscreen_settings);
+
+        mResolver = getActivity().getContentResolver();
+
+        // Determine which user is logged in
+        mIsPrimary = UserHandle.myUserId() == UserHandle.USER_OWNER;
+        if (mIsPrimary) {
+            // Its the primary user, show all the settings
+            boolean isbatteryStatusRingEnabled = Settings.System.getInt(mResolver,
+                   Settings.System.LOCKSCREEN_SHOW_BATTERY_STATUS_RING, 0) == 1;
+            mShowBatteryStatusRing = (CheckBoxPreference) findPreference(KEY_LOCKSCREEN_SHOW_BATTERY_STATUS_RING);
+            mShowBatteryStatusRing.setChecked(isbatteryStatusRingEnabled);
+            mShowBatteryStatusRing.setOnPreferenceChangeListener(this);
+
+            // Remove battery status ring style screen depending on enabled states
+            if (!isbatteryStatusRingEnabled) {
+                removePreference("lockscreen_battery_status_ring_style");
+            }
+        } else {
+            // Secondary user is logged in, remove all primary user specific preferences
+            removePreference(KEY_LOCKSCREEN_SHOW_BATTERY_STATUS_RING);
+            removePreference("lockscreen_battery_status_ring_style");
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+
+        if (preference == mShowBatteryStatusRing) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(mResolver,
+                    Settings.System.LOCKSCREEN_SHOW_BATTERY_STATUS_RING, value ? 1 : 0);
+            refreshSettings();
+            return true;
+        }
+
+        return false;
+    }
+}
