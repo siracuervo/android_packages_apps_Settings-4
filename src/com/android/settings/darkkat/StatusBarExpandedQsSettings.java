4baff288629f6dcd8c05e79d51e7854799dca10c
XXMrHyde <xxmrhyde@gmx.co.uk>
2015-01-25 00:40:56 +0100

    Quick settings customizations, (2/2):
    Based on CyanogenMod:
    
    Settings: add quick-quick settings pulldown setting:
    https://github.com/CyanogenMod/android_packages_apps_Settings/commit/be087a53d6f6b98371cff4f1cd58417fe13a03a3
    
    Add customization for Quick Settings [2/2]:
    https://github.com/CyanogenMod/android_packages_apps_Settings/commit/e8a6419e0dfafb4060b9ba8d76c75dca9ec65ae0
    
    QS: change location sensors mode in quick settings [2/2]:
    https://github.com/CyanogenMod/android_packages_apps_Settings/commit/1f07283a2f8cbc3ea633a1f44cab359bb14c8780
    
    Fix bug on QS selection preference:
    https://github.com/CyanogenMod/android_packages_apps_Settings/commit/b7beec3f4b8eac7c34fedde826ae62e00a3d8f87
    
    Quick settings: Option to disable brightness slider (2/2):
    https://github.com/CyanogenMod/android_packages_apps_Settings/commit/4539716ff8962d1948631ef0bf5c6f2b067312e3
    
    Additions:
    
    - Custom background color
    - Custom icon color
    - Custom text color,
      (this won`t change any text which is using the accent color)
    
    - Remove "Bars" deshboard tile
    - Add "Status bar" deshboard tile, (was at old "Bars" tile)
    - Add "Status bar expanded" deshboard tile, (was at old "Bars" tile)
      (icons from CyanogenMod)
    - Show "Status bar", "Status bar expanded", "More" and "Buttons" tiles for restricted users, too
    
    Conflicts:
    	Android.mk

---
+++ src/com/android/settings/darkkat/StatusBarExpandedQsSettings.java
@@ -0,0 +1,315 @@
+/*
+ * Copyright (C) 2015 DarkKat
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.darkkat;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.darkkat.qs.QSTiles;
+
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
+public class StatusBarExpandedQsSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String PREF_QS_QUICK_PULLDOWN =
+            "qs_quick_pulldown";
+    private static final String PREF_QS_ORDER =
+            "qs_order";
+    private static final String PREF_QS_MAIN_TILES =
+            "qs_main_tiles";
+    private static final String PREF_QS_LOCATION_ADVANCED =
+            "qs_location_advanced";
+    private static final String PREF_QS_SHOW_BRIGHTNESS_SLIDER =
+            "qs_show_brightness_slider";
+    private static final String PREF_QS_BACKGROUND_COLOR =
+            "qs_background_color";
+    private static final String PREF_QS_ICON_COLOR =
+            "qs_icon_color";
+    private static final String PREF_QS_TEXT_COLOR =
+            "qs_text_color";
+
+    private static final int DEFAULT_BACKGROUND_COLOR = 0xff263238;
+    private static final int WHITE = 0xffffffff;
+    private static final int HOLO_BLUE_LIGHT = 0xff33b5e5;
+
+    private static final int MENU_RESET = Menu.FIRST;
+    private static final int DLG_RESET = 0;
+
+    private CheckBoxPreference mQSQuickPulldown;
+    private Preference mQSTiles;
+    private CheckBoxPreference mQSMainTiles;
+    private CheckBoxPreference mQSLocationAdvanced;
+    private CheckBoxPreference mQSShowBrightnessSlider;
+    private ColorPickerPreference mQSBackgroundColor;
+    private ColorPickerPreference mQSIconColor;
+    private ColorPickerPreference mQSTextColor;
+
+    private ContentResolver mResolver;
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        refreshSettings();
+    }
+
+    public void refreshSettings() {
+        PreferenceScreen prefs = getPreferenceScreen();
+        if (prefs != null) {
+            prefs.removeAll();
+        }
+
+        addPreferencesFromResource(R.xml.status_bar_expanded_qs_settings);
+        mResolver = getActivity().getContentResolver();
+
+        int intColor;
+        String hexColor;
+
+        mQSQuickPulldown =
+                (CheckBoxPreference) findPreference(PREF_QS_QUICK_PULLDOWN);
+        mQSQuickPulldown.setChecked(Settings.System.getInt(mResolver,
+                Settings.System.QS_QUICK_PULLDOWN, 0) == 1);
+        mQSQuickPulldown.setOnPreferenceChangeListener(this);
+
+        mQSTiles = findPreference(PREF_QS_ORDER);
+
+        mQSMainTiles =
+                (CheckBoxPreference) findPreference(PREF_QS_MAIN_TILES);
+        mQSMainTiles.setChecked(Settings.System.getInt(mResolver,
+                Settings.System.QS_USE_MAIN_TILES, 1) == 1);
+        mQSMainTiles.setOnPreferenceChangeListener(this);
+
+        mQSLocationAdvanced =
+                (CheckBoxPreference) findPreference(PREF_QS_LOCATION_ADVANCED);
+        mQSLocationAdvanced.setChecked(Settings.System.getInt(mResolver,
+                Settings.System.QS_LOCATION_ADVANCED, 0) == 1);
+        mQSLocationAdvanced.setOnPreferenceChangeListener(this);
+
+        mQSShowBrightnessSlider =
+                (CheckBoxPreference) findPreference(PREF_QS_SHOW_BRIGHTNESS_SLIDER);
+        mQSShowBrightnessSlider.setChecked(Settings.System.getInt(mResolver,
+                Settings.System.QS_SHOW_BRIGHTNESS_SLIDER, 1) == 1);
+        mQSShowBrightnessSlider.setOnPreferenceChangeListener(this);
+
+        mQSBackgroundColor =
+                (ColorPickerPreference) findPreference(PREF_QS_BACKGROUND_COLOR);
+        intColor = Settings.System.getInt(mResolver,
+                Settings.System.QS_BACKGROUND_COLOR,
+                DEFAULT_BACKGROUND_COLOR); 
+        mQSBackgroundColor.setNewPreviewColor(intColor);
+        hexColor = String.format("#%08x", (0xffffffff & intColor));
+        mQSBackgroundColor.setSummary(hexColor);
+        mQSBackgroundColor.setAlphaSliderEnabled(true);
+        mQSBackgroundColor.setOnPreferenceChangeListener(this);
+
+        mQSIconColor =
+                (ColorPickerPreference) findPreference(PREF_QS_ICON_COLOR);
+        intColor = Settings.System.getInt(mResolver,
+                Settings.System.QS_ICON_COLOR, WHITE); 
+        mQSIconColor.setNewPreviewColor(intColor);
+        hexColor = String.format("#%08x", (0xffffffff & intColor));
+        mQSIconColor.setSummary(hexColor);
+        mQSIconColor.setOnPreferenceChangeListener(this);
+
+        mQSTextColor =
+                (ColorPickerPreference) findPreference(PREF_QS_TEXT_COLOR);
+        intColor = Settings.System.getInt(mResolver,
+                Settings.System.QS_TEXT_COLOR, WHITE); 
+        mQSTextColor.setNewPreviewColor(intColor);
+        hexColor = String.format("#%08x", (0xffffffff & intColor));
+        mQSTextColor.setSummary(hexColor);
+        mQSTextColor.setOnPreferenceChangeListener(this);
+
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        int qsTileCount = QSTiles.determineTileCount(getActivity());
+        mQSTiles.setSummary(getResources().getQuantityString(R.plurals.qs_tiles_summary,
+                    qsTileCount, qsTileCount));
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.add(0, MENU_RESET, 0, R.string.reset)
+                .setIcon(R.drawable.ic_menu_reset) // use the KitKat backup icon
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_RESET:
+                showDialogInner(DLG_RESET);
+                return true;
+             default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        boolean value;
+        String hex;
+        int intHex;
+
+        if (preference == mQSQuickPulldown) {
+            value = (Boolean) newValue;
+            Settings.System.putInt(mResolver,
+                Settings.System.QS_QUICK_PULLDOWN, value ? 1 : 0);
+            return true;
+        } else if (preference == mQSMainTiles) {
+            value = (Boolean) newValue;
+            Settings.System.putInt(mResolver,
+                Settings.System.QS_USE_MAIN_TILES, value ? 1 : 0);
+            return true;
+        } else if (preference == mQSLocationAdvanced) {
+            value = (Boolean) newValue;
+            Settings.System.putInt(mResolver,
+                Settings.System.QS_LOCATION_ADVANCED, value ? 1 : 0);
+            return true;
+        } else if (preference == mQSShowBrightnessSlider) {
+            value = (Boolean) newValue;
+            Settings.System.putInt(mResolver,
+                Settings.System.QS_SHOW_BRIGHTNESS_SLIDER, value ? 1 : 0);
+            return true;
+        } else if (preference == mQSBackgroundColor) {
+            hex = ColorPickerPreference.convertToARGB(
+                Integer.valueOf(String.valueOf(newValue)));
+            intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(mResolver,
+                Settings.System.QS_BACKGROUND_COLOR, intHex);
+            preference.setSummary(hex);
+            return true;
+        } else if (preference == mQSIconColor) {
+            hex = ColorPickerPreference.convertToARGB(
+                Integer.valueOf(String.valueOf(newValue)));
+            intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(mResolver,
+                Settings.System.QS_ICON_COLOR, intHex);
+            preference.setSummary(hex);
+            return true;
+        } else if (preference == mQSTextColor) {
+            hex = ColorPickerPreference.convertToARGB(
+                Integer.valueOf(String.valueOf(newValue)));
+            intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(mResolver,
+                Settings.System.QS_TEXT_COLOR, intHex);
+            preference.setSummary(hex);
+            return true;
+        }
+        return false;
+    }
+
+    private void showDialogInner(int id) {
+        DialogFragment newFragment = MyAlertDialogFragment.newInstance(id);
+        newFragment.setTargetFragment(this, 0);
+        newFragment.show(getFragmentManager(), "dialog " + id);
+    }
+
+    public static class MyAlertDialogFragment extends DialogFragment {
+
+        public static MyAlertDialogFragment newInstance(int id) {
+            MyAlertDialogFragment frag = new MyAlertDialogFragment();
+            Bundle args = new Bundle();
+            args.putInt("id", id);
+            frag.setArguments(args);
+            return frag;
+        }
+
+        StatusBarExpandedQsSettings getOwner() {
+            return (StatusBarExpandedQsSettings) getTargetFragment();
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            int id = getArguments().getInt("id");
+            switch (id) {
+                case DLG_RESET:
+                    return new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.reset)
+                    .setMessage(R.string.dlg_reset_values_message)
+                    .setNegativeButton(R.string.cancel, null)
+                    .setNeutralButton(R.string.dlg_reset_android,
+                        new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.QS_QUICK_PULLDOWN, 0);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.QS_USE_MAIN_TILES, 1);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.QS_LOCATION_ADVANCED, 0);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.QS_SHOW_BRIGHTNESS_SLIDER, 1);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.QS_BACKGROUND_COLOR,
+                                    DEFAULT_BACKGROUND_COLOR);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.QS_ICON_COLOR, WHITE);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.QS_TEXT_COLOR, WHITE);
+                            getOwner().refreshSettings();
+                        }
+                    })
+                    .setPositiveButton(R.string.dlg_reset_darkkat,
+                        new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.QS_QUICK_PULLDOWN, 1);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.QS_USE_MAIN_TILES, 0);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.QS_LOCATION_ADVANCED, 1);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.QS_SHOW_BRIGHTNESS_SLIDER, 1);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.QS_BACKGROUND_COLOR,
+                                    0xff1b1f23);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.QS_ICON_COLOR,
+                                    HOLO_BLUE_LIGHT);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.QS_TEXT_COLOR,
+                                    HOLO_BLUE_LIGHT);
+                            getOwner().refreshSettings();
+                        }
+                    })
+                    .create();
+            }
+            throw new IllegalArgumentException("unknown id " + id);
+        }
+
+        @Override
+        public void onCancel(DialogInterface dialog) {
+
+        }
+    }
+}
